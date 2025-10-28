62b7aa67039c62c936e1297773230e451ca0b440
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-10-28 15:06:22 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
     pass
+a = int(input('вывод a:'))
+b = int(input('вывод b:'))
+for i in range (a,b+1):
+    print(i)
+n = b - a + 1
+print('количество этих чисел n =',n)
