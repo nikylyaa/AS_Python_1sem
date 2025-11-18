b9939b8e62eb7bbd8b4cc7b9f4968feb6c6b930b
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-11-18 12:31:00 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,22 @@
-if __name__ == "__main__":
-    pass
+a) try:
+    n = int(input('число:'))
+    if n < 0 : raise ValueError('отрицательное число')
+    fac = 1
+    for i in range(1,n + 1 ): fac *= i
+    print('факториал = ', fac)
+except ValueError as e: print (e)
+b) try:
+    n = int(input('число:'))
+    if n <= 0: raise ValueError('число не является положительным')
+    print('число является положительным')
+except ValueError as e:
+    print(e)
+c) try:
+    n = int(input('число:'))
+    assert n>=0, 'отрицательное число'
+    fac = 1
+    for i in range(1,n + 1):
+        fac *= i
+    print('факториал = ',fac)
+except ValueError as e:
+    print(e)
