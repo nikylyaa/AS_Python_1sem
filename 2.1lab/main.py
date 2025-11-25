2ebc5e6bed666724e606adbfa0b075447d77f64e
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-11-25 15:01:22 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    pass
+d = {'a':1, 'b':2, 'c':3, 'd':4}
+lst = [{'a':1}, {'a':2}, {'a':3}, {'b':1}]
+students = {'Иван':{'рост':170,'вес':70}, 'Михаил':{'рост':180,'вес':75}}
+print('а)')
+# а)
+for k, v in d.items():
+    print(f"key: {k}, value: {v}")
+# б)
+print('б)')
+print("Сумма по 'a':", sum(x.get('a',0) for x in lst))
+# в)
+print('в)')
+print({n:{'рост':info['рост']} for n,info in students.items()})
