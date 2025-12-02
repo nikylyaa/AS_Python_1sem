5e8e4bf7b0c452477d74dd3896174b0af1f7e3b2
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-12-02 12:27:30 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+def InvertStr(S, K, N):
+  K-=1
+  if K>= len(S):
+      return ''
+  return S[K:K+N][::-1]
+InvertStr("abcdef", 2, 3)
+InvertStr("abcdef", 6, 10)
+InvertStr("abcdef", 10, 3)  
