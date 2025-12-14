96f9668a0470dc1f6c651c7826b9eb15160129ec
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-12-15 02:27:02 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+def Simm(S):
+    if len(S) <= 1:
+        return True
+    if S[0] != S[-1]:
+        return False
+    return Simm
+text = "radar" #ввод любого слова
+if Simm(text):
+    print("Строка симметрична")
+else:
+    print("Строка несимметрична")
