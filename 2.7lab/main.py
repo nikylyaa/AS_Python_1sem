75b7949a202cda68925b9114f344996e94ea86b5
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-12-15 02:55:19 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    pass
+r = open("input.txt", "r")
+w = open("output.txt", "w")
+
+for line in r:
+    line = line.rstrip("\n")
+
+    if line != "":
+        if len(line) % 2 != 0:
+            line = " " + line
+
+        spaces = (50 - len(line)) // 2
+        line = " " * spaces + line
+
+    w.write(line + "\n")
+
+r.close()
+w.close()
