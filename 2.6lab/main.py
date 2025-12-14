e726ea367ccb3d1a2ddb1d3d5c7dd0bdd62ab671
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-12-15 02:45:46 +0300

    Update main.py

--- 2.6lab/main.py
+++ 2.6lab/main.py
@@ -1,2 +1,26 @@
-if __name__ == "__main__":
-    pass
+# удаление цифр из файла
+def remove_digits(filename):
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+    result = ""
+    for smvl in text:
+        if smvl < '0' or smvl > '9':
+            result = result + smvl
+    f = open(filename, 'w')
+    f.write(result)
+    f.close()
+    
+# подсчёт символов в файле
+def count_smvl(filename):
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+    return len(text)
+
+# подсчёт строк в файле
+def count_lines(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
+    return len(lines)
