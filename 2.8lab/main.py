7e80e980878ed5a282fd35045cdcbabdae1f4fb8
nikylyaa <88z7y6gt8c@privaterelay.appleid.com>
2025-12-15 03:00:11 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+r = open("f.txt", "r")
+text = r.read()
+r.close()
+numbers = text.split()
+for i in range(len(numbers)):
+    numbers[i] = float(numbers[i])
+max_value = 0
+for i in range(0, len(numbers), 2):
+    if abs(numbers[i]) > max_value:
+        max_value = abs(numbers[i])
+print("Максимальный модуль среди нечётных номеров:", max_value)
